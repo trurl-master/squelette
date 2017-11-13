ea39dd60089b23caf93ed6d56beaa8bbcfe53aeb
Ivan Galyatin <trurl-master@ya.ru>
2017-11-13 12:52:01 +0300

    Notify if meta record doesn't exist

--- inc/controllers/default.php
+++ inc/controllers/default.php
@@ -1,9 +1,15 @@
 <?php
 
+$meta = \Squelette\MetaQuery::create()->filterByName($page)->findOne();
+
+if ($meta === null && !App::cfg('is_production')) {
+    echo 'warning: meta record “', $page, '” doesn\'t exist';
+}
+
 App::renderTemplate(
     'master',
     [
     	'page' => $page,
-    	'meta' => \Squelette\MetaQuery::create()->filterByName($page)->findOne()
+    	'meta' => $meta
     ]
 );
